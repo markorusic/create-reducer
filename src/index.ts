285bc723397931d5f6f63bdbfaec4718ee1c2da2
Marko Rusic <markousp5@gmail.com>
2019-08-22 21:13:47 +0200

    added types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import merge from 'lodash.merge'
 
-const createReducer = (handlers, initialState = {}) => (
-  state = initialState,
-  action
-) => {
+interface Action<Payload = any> {
+  type: string
+  payload?: Payload
+}
+
+type Handler<State> = (state: State, action: Action) => Partial<State>
+
+interface HandlerGroup<State> {
+  [key: string]: Handler<State>
+}
+
+const createReducer = <State>(
+  handlers: HandlerGroup<State>,
+  initialState?: State
+) => (state = initialState, action: Action) => {
   const handler = handlers[action.type]
   if (!handler) {
     throw new Error(`Unknown action type: ${action.type}`)
